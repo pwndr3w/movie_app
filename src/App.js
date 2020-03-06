b2bb7878505234ef7385a28a16ba3cb42f6c6675
김민성 <minsung@gimminseong-ui-MacBookPro.local>
2020-03-07 00:35:34 +0900

    aws initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      Edit <code>src/App.js</code> and save to reload Part 2.
+      <h1>Hello</h1>
+      <Food fav="kimchi"/>
     </div>
+  
   );
 }
 
